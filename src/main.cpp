2fc36b867b2188e113e1a0f456d20f9bc34ce1a3
navyavaishnavi <navyavaishu2123@gmail.com>
2025-11-28 11:58:06 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include "tracker.h"
 using namespace std;
 
 int main() {
-    cout << "✅ Tracker program is running successfully!" << endl;
-    system("pause");
+    int choice;
+    string topic;
+    int problems;
+
+    while (true) {
+        cout << "\n==== DSA Progress Tracker ====\n";
+        cout << "1. Add Progress\n";
+        cout << "2. View Progress\n";
+        cout << "3. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            cout << "Enter Topic Name: ";
+            cin >> topic;
+            cout << "Problems Solved: ";
+            cin >> problems;
+            addProgress(topic, problems);
+        }
+        else if (choice == 2) {
+            viewProgress();
+        }
+        else if (choice == 3) {
+            break;
+        }
+        else {
+            cout << "Invalid choice!\n";
+        }
+    }
     return 0;
 }
+
 
 
 
