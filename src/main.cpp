067e7701fb2c3736f07fe0f6066e84d732acd3f7
navyavaishnavi <navyavaishu2123@gmail.com>
2025-11-28 11:30:51 +0530

    Implement DSA Progress Tracker menu
    Added a menu for tracking DSA progress with options to add and view progress.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include "tracker.h"
 using namespace std;
 
 int main() {
-    cout << "DSA Progress Tracker Initialized!" << endl;
+    int choice;
+    string topic;
+    int solved;
+
+    do {
+        cout << "\n--- DSA Progress Tracker ---\n";
+        cout << "1. Add Progress\n";
+        cout << "2. View Progress\n";
+        cout << "3. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter Topic: ";
+                cin >> topic;
+                cout << "Problems Solved: ";
+                cin >> solved;
+                addProgress(topic, solved);
+                break;
+
+            case 2:
+                viewProgress();
+                break;
+
+            case 3:
+                cout << "Exiting...\n";
+                break;
+
+            default:
+                cout << "Invalid choice\n";
+        }
+    } while (choice != 3);
+
     return 0;
 }
+
